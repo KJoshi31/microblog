d5753efa1eb9d4321a32456c7ad2887fe7b60699
root <root@LAPTOP-A4VHHFN5.localdomain>
2018-09-12 19:32:09 -0500

    first commit

---
+++ app/routes.py
@@ -0,0 +1,44 @@
+from app import app
+from flask import render_template
+from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username':'Karan'}
+
+    posts = [
+        {
+            'author':{'username':'John'},
+            'body':'Beautiful day in Portland!'
+        },
+        {
+            'author':{'username':'Susan'},
+            'body':'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html',title="Home",user=user,posts=posts)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    pythonForm = LoginForm()
+
+    if pythonForm.validate_on_submit():
+
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+
+    return render_template('login.html',title='Sign In', form=pythonForm)
